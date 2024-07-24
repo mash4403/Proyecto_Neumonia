ed9a12e524c66cbfaa6d65b7084e4d0c027c6577
mash4403 <msaavedrah@gmail.com>
2024-07-24 09:51:19 -0500

    Se actualiza para  mejorar la estructura del proyecto

--- detector_neumonia.py
+++ detector_neumonia.py
@@ -1,116 +1,21 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from tkinter import *
-from tkinter import ttk, font, filedialog, Entry
-
+from tkinter import ttk, font, filedialog
 from tkinter.messagebox import askokcancel, showinfo, WARNING
-import getpass
 from PIL import ImageTk, Image
 import csv
-import pyautogui
 import tkcap
-import img2pdf
-import numpy as np
-import time
-tf.compat.v1.disable_eager_execution()
-tf.compat.v1.experimental.output_all_intermediates(True)
-import cv2
-
-
-def grad_cam(array):
-    img = preprocess(array)
-    model = model_fun()
-    preds = model.predict(img)
-    argmax = np.argmax(preds[0])
-    output = model.output[:, argmax]
-    last_conv_layer = model.get_layer("conv10_thisone")
-    grads = K.gradients(output, last_conv_layer.output)[0]
-    pooled_grads = K.mean(grads, axis=(0, 1, 2))
-    iterate = K.function([model.input], [pooled_grads, last_conv_layer.output[0]])
-    pooled_grads_value, conv_layer_output_value = iterate(img)
-    for filters in range(64):
-        conv_layer_output_value[:, :, filters] *= pooled_grads_value[filters]
-    # creating the heatmap
-    heatmap = np.mean(conv_layer_output_value, axis=-1)
-    heatmap = np.maximum(heatmap, 0)  # ReLU
-    heatmap /= np.max(heatmap)  # normalize
-    heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[2]))
-    heatmap = np.uint8(255 * heatmap)
-    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-    img2 = cv2.resize(array, (512, 512))
-    hif = 0.8
-    transparency = heatmap * hif
-    transparency = transparency.astype(np.uint8)
-    superimposed_img = cv2.add(transparency, img2)
-    superimposed_img = superimposed_img.astype(np.uint8)
-    return superimposed_img[:, :, ::-1]
-
-
-def predict(array):
-    #   1. call function to pre-process image: it returns image in batch format
-    batch_array_img = preprocess(array)
-    #   2. call function to load model and predict: it returns predicted class and probability
-    model = model_fun()
-    # model_cnn = tf.keras.models.load_model('conv_MLP_84.h5')
-    prediction = np.argmax(model.predict(batch_array_img))
-    proba = np.max(model.predict(batch_array_img)) * 100
-    label = ""
-    if prediction == 0:
-        label = "bacteriana"
-    if prediction == 1:
-        label = "normal"
-    if prediction == 2:
-        label = "viral"
-    #   3. call function to generate Grad-CAM: it returns an image with a superimposed heatmap
-    heatmap = grad_cam(array)
-    return (label, proba, heatmap)
-
-
-def read_dicom_file(path):
-    img = dicom.read_file(path)
-    img_array = img.pixel_array
-    img2show = Image.fromarray(img_array)
-    img2 = img_array.astype(float)
-    img2 = (np.maximum(img2, 0) / img2.max()) * 255.0
-    img2 = np.uint8(img2)
-    img_RGB = cv2.cvtColor(img2, cv2.COLOR_GRAY2RGB)
-    return img_RGB, img2show
-
-
-def read_jpg_file(path):
-    img = cv2.imread(path)
-    img_array = np.asarray(img)
-    img2show = Image.fromarray(img_array)
-    img2 = img_array.astype(float)
-    img2 = (np.maximum(img2, 0) / img2.max()) * 255.0
-    img2 = np.uint8(img2)
-    return img2, img2show
-
-
-def preprocess(array):
-    array = cv2.resize(array, (512, 512))
-    array = cv2.cvtColor(array, cv2.COLOR_BGR2GRAY)
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4, 4))
-    array = clahe.apply(array)
-    array = array / 255
-    array = np.expand_dims(array, axis=-1)
-    array = np.expand_dims(array, axis=0)
-    return array
-
+from integrator import predict
+from read_img import read_dicom_file
 
 class App:
     def __init__(self):
         self.root = Tk()
         self.root.title("Herramienta para la detección rápida de neumonía")
-
-        #   BOLD FONT
         fonti = font.Font(weight="bold")
-
         self.root.geometry("815x560")
         self.root.resizable(0, 0)
 
-        #   LABELS
+        # Labels
         self.lab1 = ttk.Label(self.root, text="Imagen Radiográfica", font=fonti)
         self.lab2 = ttk.Label(self.root, text="Imagen con Heatmap", font=fonti)
         self.lab3 = ttk.Label(self.root, text="Resultado:", font=fonti)
@@ -122,23 +27,18 @@ class App:
         )
         self.lab6 = ttk.Label(self.root, text="Probabilidad:", font=fonti)
 
-        #   TWO STRING VARIABLES TO CONTAIN ID AND RESULT
+        # String Variables
         self.ID = StringVar()
         self.result = StringVar()
 
-        #   TWO INPUT BOXES
+        # Input Boxes
         self.text1 = ttk.Entry(self.root, textvariable=self.ID, width=10)
-
-        #   GET ID
-        self.ID_content = self.text1.get()
-
-        #   TWO IMAGE INPUT BOXES
         self.text_img1 = Text(self.root, width=31, height=15)
         self.text_img2 = Text(self.root, width=31, height=15)
         self.text2 = Text(self.root)
         self.text3 = Text(self.root)
 
-        #   BUTTONS
+        # Buttons
         self.button1 = ttk.Button(
             self.root, text="Predecir", state="disabled", command=self.run_model
         )
@@ -151,7 +51,7 @@ class App:
             self.root, text="Guardar", command=self.save_results_csv
         )
 
-        #   WIDGETS POSITIONS
+        # Widget Positions
         self.lab1.place(x=110, y=65)
         self.lab2.place(x=545, y=65)
         self.lab3.place(x=500, y=350)
@@ -169,19 +69,15 @@ class App:
         self.text_img1.place(x=65, y=90)
         self.text_img2.place(x=500, y=90)
 
-        #   FOCUS ON PATIENT ID
+        # Focus on Patient ID
         self.text1.focus_set()
 
-        #  se reconoce como un elemento de la clase
+        # Recognize as class element
         self.array = None
-
-        #   NUMERO DE IDENTIFICACIÓN PARA GENERAR PDF
         self.reportID = 0
-
-        #   RUN LOOP
         self.root.mainloop()
 
-    #   METHODS
+    # Methods
     def load_img_file(self):
         filepath = filedialog.askopenfilename(
             initialdir="/",
@@ -205,7 +101,6 @@ class App:
         self.img2 = Image.fromarray(self.heatmap)
         self.img2 = self.img2.resize((250, 250), Image.ANTIALIAS)
         self.img2 = ImageTk.PhotoImage(self.img2)
-        print("OK")
         self.text_img2.image_create(END, image=self.img2)
         self.text2.insert(END, self.label)
         self.text3.insert(END, "{:.2f}".format(self.proba) + "%")
@@ -241,11 +136,5 @@ class App:
             self.text_img2.delete(self.img2, "end")
             showinfo(title="Borrar", message="Los datos se borraron con éxito")
 
-
-def main():
-    my_app = App()
-    return 0
-
-
 if __name__ == "__main__":
-    main()
+    app = App()
